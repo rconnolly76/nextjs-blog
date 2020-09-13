b0058c8478da9ce155b6804c94af6e6c2b5e8f82
Roger Connolly <Roger@The-Connollys-iMac.local>
2020-09-13 22:40:31 +0200

    fix2

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Roger Connolly'
+const name = 'CaaS and Headless for Dummies'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
